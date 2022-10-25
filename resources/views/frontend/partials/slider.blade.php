363659130d108ad3920f5debf04c003aaf851b34
OmarXtream <48177756+OmarXtream@users.noreply.github.com>
2022-10-25 15:51:41 +0300

    Slider with DashBoard

--- resources/views/frontend/partials/slider.blade.php
+++ resources/views/frontend/partials/slider.blade.php
@@ -1,11 +1,9 @@
 <section class="banner-style-three rtl">
     <div class="banner-carousel owl-theme owl-carousel owl-nav-none owl-dots-none">
+
         <div class="slide-item rtl">
             <div class="pattern-box">
                 <div class="pattern-1"></div>
-                <div class="pattern-2" style="background-image: url({{asset('https://k.top4top.io/p_246729yd21.jpg')}});"></div>
-                <div class="pattern-3" style="background-image: url({{asset('https://k.top4top.io/p_246729yd21.jpg')}});"></div>
-                <div class="pattern-4" style="background-image: url({{asset('https://k.top4top.io/p_246729yd21.jpg')}});"></div>
             </div>
             <div class="auto-container">
                 <div class="inner-box">
@@ -21,62 +19,25 @@
             </div>
         </div>
 
+        @if($sliders)
+        @foreach($sliders as $slider)
+
         <div class="slide-item rtl">
             <div class="pattern-box">
                 <div class="pattern-1"></div>
-                <div class="pattern-2" style="background-image: url({{asset('https://k.top4top.io/p_2467os9iq1.jpg')}});"></div>
-                <div class="pattern-3" style="background-image: url({{asset('https://k.top4top.io/p_2467os9iq1.jpg')}});"></div>
-                <div class="pattern-4" style="background-image: url({{asset('https://k.top4top.io/p_2467os9iq1.jpg')}});"></div>
             </div>
             <div class="auto-container">
                 <div class="inner-box">
                     <div class="content-box">
-                        <h2>أفضل خيار للعقار !</h2>
-                        <p>مع الروابي يمكنك ان تشتري او تبيع عقاراتك بكل سهولة.</p>
-                        <div class="button-box">
-                            <a href="{{route('property')}}" class="theme-btn btn-one">البحث عن عقار</a>
-                        </div>
+                        <h2>{{ $slider->title }} </h2>
+                        <p>{{ $slider->description }}</p>
                     </div>
-                    <figure class="image-box"><img src="https://k.top4top.io/p_2467os9iq1.jpg" alt=""></figure>
+                    <figure class="image-box"><img src="{{Storage::url('slider/'.$slider->image)}}" alt=""></figure>
                 </div> 
             </div>
         </div>
+        @endforeach
+        @endif
 
     </div>
 </section>
-
-{{--         
-        <!-- banner-style-two -->
-        <section class="banner-style-two centred">
-            <div class="banner-carousel owl-theme owl-carousel owl-nav-none">
-
-                @if($sliders)
-                @foreach($sliders as $slider)
-
-                <div class="slide-item">
-                    <div class="image-layer" style="background-image:url({{Storage::url('slider/'.$slider->image)}})"></div>
-                    <div class="auto-container">
-                        <div class="content-box">
-                            <h2>{{ $slider->title }}</h2>
-                            <p>{{ $slider->description }}</p>
-                        </div>  
-                    </div>
-                </div>
-    
-                @endforeach
-            @else 
-
-            <div class="slide-item">
-                <div class="image-layer" style="background-image:url({{ asset('frontend/images/real_estate.jpg') }})"></div>
-                <div class="auto-container">
-                    <div class="content-box">
-                        <h2>Slider Title Here</h2>
-                        <p>Slider description Here.</p>
-                    </div>  
-                </div>
-            </div>
-            @endif
-        
-            </div>
-        </section>
-        <!-- banner-style-two end --> --}}
