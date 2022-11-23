b892f28439e8084e735d861caa8ad8e5062c0a94
OmarXtream <48177756+OmarXtream@users.noreply.github.com>
2022-11-23 20:44:33 +0300

    img-fluid

--- resources/views/pages/blog/index.blade.php
+++ resources/views/pages/blog/index.blade.php
@@ -28,7 +28,7 @@
                         <div class="news-block-two wow fadeInLeft animated animated" data-wow-delay="00ms" data-wow-duration="1500ms">
                             <div class="inner-box align-items-center">
                                 <div class="image-box">
-                                    <figure class="image"><a href="{{ route('blog.show',$post->slug) }}"><img src="{{Storage::url('posts/'.$post->image)}}" alt="{{$post->title}}"></a></figure>
+                                    <figure class="image"><a href="{{ route('blog.show',$post->slug) }}"><img class="img-fluid" src="{{Storage::url('posts/'.$post->image)}}" alt="{{$post->title}}"></a></figure>
                                     <a href="{{ route('blog.show',$post->slug) }}" class="feature">تدوينة حديثه</a>
                                 </div>
                                 <div class="content-box">
