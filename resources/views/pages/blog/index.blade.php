b4a23c20c19a47784e6f7939ad10a57849d83c24
OmarXtream <48177756+OmarXtream@users.noreply.github.com>
2022-11-23 15:04:47 +0300

    Blog List New Design

--- resources/views/pages/blog/index.blade.php
+++ resources/views/pages/blog/index.blade.php
@@ -7,44 +7,53 @@
 
 @section('content')
 
-    <section class="section">
-        <div class="container">
-            <div class="row">
-                <h4 class="section-heading">منشورات المدونة</h4>
-            </div>
-            <div class="row">
+<section class="page-title centred" style="background-image: url(frontend/images/shape/shape-9.png);">
+    <div class="auto-container">
+        <div class="content-box clearfix">
+            <h1>منشورات المدونة</h1>
+            <ul class="bread-crumb clearfix">
+                <li><a href="/">الرئيسية</a></li>
+            </ul>
+        </div>
+    </div>
+</section>
+          
 
-                <div class="container">
-                    <div class="row">
-                  
+                            <!-- sidebar-page-container -->
+        <section class="blog-list sec-pad-2">
+            <div class="auto-container">
+                <div class="row clearfix">
                     @foreach($posts as $post)
-                    <div class="col-4 s12 m8 mb-2">
-                    <div class="card" style="width: 18rem;">
-                        @if(Storage::disk('public')->exists('posts/'.$post->image) && $post->image)
-                        <img class="card-img-top" src="{{Storage::url('posts/'.$post->image)}}" alt="{{$post->title}}">
-                        @endif
-
-                        <div class="card-body">
-                          <h5 class="card-title">{{ $post->title }}</h5>
-                          <p class="card-text">{!! str_limit($post->body,120) !!}</p>
-                          <a href="{{ route('blog.show',$post->slug) }}" class="btn btn-primary">تفاصيل أكثر</a>
+                    <div class="col-lg-6 col-md-12 col-sm-12 news-block">
+                        <div class="news-block-two wow fadeInLeft animated animated" data-wow-delay="00ms" data-wow-duration="1500ms">
+                            <div class="inner-box align-items-center">
+                                <div class="image-box">
+                                    <figure class="image"><a href="{{ route('blog.show',$post->slug) }}"><img src="{{Storage::url('posts/'.$post->image)}}" alt="{{$post->title}}"></a></figure>
+                                    <a href="{{ route('blog.show',$post->slug) }}" class="feature">تدوينة حديثه</a>
+                                </div>
+                                <div class="content-box">
+                                    <h4><a href="{{ route('blog.show',$post->slug) }}">{{ $post->title }}</a></h4>
+                                    <ul class="post-info clearfix">
+                                        <li class="author-box">
+                                            <figure class="author-thumb"><img src="assets/images/news/author-1.jpg" alt=""></figure>
+                                            <h5><a href="{{ route('blog.show',$post->slug) }}">Eva Green</a></h5>
+                                        </li>
+                                        <li>{{$post->created_at}}</li>
+                                    </ul>
+                                    <p>{!! str_limit($post->body,120) !!}</p>
+                                    <div class="btn-box">
+                                        <a href="{{ route('blog.show',$post->slug) }}" class="theme-btn btn-two">تفاصيل أكثر</a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                      </div>
                     </div>
                     @endforeach
 
-
-                    <div class="m-t-30 m-b-60 center">
-                        {{ $posts->appends(['month' => Request::get('month'), 'year' => Request::get('year')])->links() }}
-                    </div>
-        
                 </div>
             </div>
+        </section>
 
-
-            </div>
-        </div>
-    </section>
 
 @endsection
 
