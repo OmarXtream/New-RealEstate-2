dba0d99803aaec4f22b07ae5104917390f9a51ab
OmarXtream <48177756+OmarXtream@users.noreply.github.com>
2022-11-23 21:58:45 +0300

    Update index.blade.php

--- resources/views/pages/blog/index.blade.php
+++ resources/views/pages/blog/index.blade.php
@@ -26,8 +26,8 @@
                     @foreach($posts as $post)
                     <div class="col-lg-6 col-md-12 col-sm-12 news-block">
                         <div class="news-block-two wow fadeInLeft animated animated" data-wow-delay="00ms" data-wow-duration="1500ms">
-                            <div class="inner-box align-items-center">
-                                <div class="image-box">
+                            <div class="inner-box align-items-center img-fluid">
+                                <div class="image-box img-fluid">
                                     <a href="{{ route('blog.show',$post->slug) }}"><img style="height: 100%" class="img-fluid" src="{{Storage::url('posts/'.$post->image)}}" alt="{{$post->title}}"></a>
                                 </div>
                                 <div class="content-box">
