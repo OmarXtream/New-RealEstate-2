d6950505dc200347879b5a1cb42bf72653f88d7f
OmarXtream <48177756+OmarXtream@users.noreply.github.com>
2022-12-03 23:20:29 +0300

    InfoForm Show error

--- resources/views/pages/infoForm.blade.php
+++ resources/views/pages/infoForm.blade.php
@@ -24,12 +24,15 @@
         <section class="contact-section">
             @if(Session::has('errors'))
             <div class="text-center alert alert-light">
-              <h5 style="font-weight: bold;color:black;">فضلاً قم بملىء كل الحقول</h5>
+                <h5 style="font-weight: bold;color:black">فضلاً قم بملىء كل الحقول</h5>
+            @if($errors->any())
+            {!! implode('', $errors->all('<p style="color:red">:message</p>')) !!}
+            @endif
             </div>
             @endif
             @if (session()->has('message'))
             <div class="text-center alert alert-light">
-                <h3 style="font-weight: bold;color:black;">{{ session('message') }}</h3>
+                <h3 style="font-weight: bold; color:black">{{ session('message') }}</h3>
             </div>
             @endif
 
