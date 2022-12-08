c6638d264c06c5b8e45321e1f3b1f3750df72871
OmarXtream <48177756+OmarXtream@users.noreply.github.com>
2022-12-09 02:12:18 +0300

    Blog Editor upload img

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -171,4 +171,29 @@ class PostController extends Controller
         Toastr::success('message', 'Post deleted successfully.');
         return back();
     }
+
+
+    public function upload(Request $request)
+    {
+        if($request->hasFile('upload')) {
+            $filenamewithextension = $request->file('upload')->getClientOriginalName();
+       
+            $filename = pathinfo($filenamewithextension, PATHINFO_FILENAME);
+       
+            $extension = $request->file('upload')->getClientOriginalExtension();
+       
+            $filenametostore = $filename.'_'.time().'.'.$extension;
+       
+            $request->file('upload')->storeAs('public/uploads', $filenametostore);
+  
+            $CKEditorFuncNum = $request->input('CKEditorFuncNum');
+            $url = asset('storage/uploads/'.$filenametostore);
+            $msg = 'Image successfully uploaded';
+            $re = "<script>window.parent.CKEDITOR.tools.callFunction($CKEditorFuncNum, '$url', '$msg')</script>";
+              
+            // Render HTML output
+            @header('Content-type: text/html; charset=utf-8');
+            echo $re;
+        }
+    }
 }
