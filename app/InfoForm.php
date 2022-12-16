6880d81c80776fa10558b9708b5fa7f145b59ca0
OmarXtream <48177756+OmarXtream@users.noreply.github.com>
2022-12-16 11:54:47 +0300

    fix age model

--- app/InfoForm.php
+++ app/InfoForm.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class InfoForm extends Model
 {
-    protected $fillable = ['id','name','phone','type','commitments','bank','salary','supported','notes'];
+    protected $fillable = ['id','name','Age','phone','type','commitments','bank','salary','supported','notes'];
 
 
     public function typeText(){
