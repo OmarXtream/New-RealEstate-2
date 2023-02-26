bd934340b1854fd93a981a9560f1c9ba91da7ab5
OmarXtream <48177756+OmarXtream@users.noreply.github.com>
2023-02-27 00:16:45 +0300

    features icon size

--- resources/views/pages/properties/single.blade.php
+++ resources/views/pages/properties/single.blade.php
@@ -312,7 +312,7 @@
                                 @foreach($property->features as $feature)
                                 <p>{{$feature->name}}
                                 @if(Storage::disk('public')->exists('features/'.$feature->icon))
-                                <img src="{{Storage::url('features/'.$feature->icon)}}" alt="{{$feature->name}}">
+                                <img src="{{Storage::url('features/'.$feature->icon)}}" width="20px" height="20px" alt="{{$feature->name}}">
                                 @endif
 
                                 </p>
